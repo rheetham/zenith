fc1e9132af7ca7c155c6be14aeb160c1c1746605
unknown <danishtiwari2204@gmail.com>
2024-02-24 13:24:30 +0530

    adding_main_page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainPage from './components/main_page';
+
+function App() {
+  return (
+    <div>
+        <MainPage />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import MainPage from './components/main_page';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <App />
   </React.StrictMode>
 );
 
